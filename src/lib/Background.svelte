0c478e35b3cbb9ba44604b54b3a489afd42d1143
saranatour1 <saranatourcontact@gmail.com>
2024-02-22 20:04:03 +0200

    TAS-65/chore:bg-size fix

--- src/lib/Background.svelte
+++ src/lib/Background.svelte
@@ -1,2 +1,2 @@
 <div class="overly fixed top-0 left-0  min-h-screen w-full bg-vlg dark:bg-vdb -z-20 transition-colors"/>
-<div class="bg-lg-light dark:bg-lg-dark h-full w-full bg-fixed absolute top-0 left-0 bg-no-repeat dark:max-sm:bg-sm-dark max-sm:bg-sm-light -z-10 transition-all" />
+<div class="bg-lg-light dark:bg-lg-dark h-fit min-h-[20rem]  w-full bg-contain bg-[auto 50%] bg-fixed absolute top-0 left-0 bg-no-repeat dark:max-sm:bg-sm-dark max-sm:bg-sm-light -z-10 transition-all inset-0 " />
